7da9792a55d7bea301293b91f9ec1777550e62be
melanie0245 <melanie-parraga@hotmail.com>
2019-10-03 01:15:59 -0700

    poj

---
+++ if/main.js
@@ -0,0 +1,43 @@
+/*var person = prompt("Please enter your name");
+var age = prompt("Please enter your age");
+alert("Hello" + person + "Next year you will be " + (Number(age) + 1));*/
+
+
+/*var x = prompt("Where do you live?")
+var correctAnswer = "Luxembourg";
+if (x === correctAnswer) {
+    alert("Correct!");
+}*/
+
+
+/*var firstnumber = prompt("Please enter the first number");
+var secondnumber = prompt("Please enter the second number");
+if (firstnumber === secondnumber) {
+    alert("They ARE equal");
+}
+if (firstnumber !== secondnumber) {
+    alert("They ARE NOT equal");
+
+}*/
+
+
+/*var age = prompt("How old are you?")
+var y = prompt("Do you have a Diploma?")
+if (age >= 20 && y == ("yes")) {
+    alert("You can work")
+
+}*/
+
+var age = prompt("How old are you?")
+var y = prompt("Do you have a Diploma?")
+if (age > 20) {
+    if (y == ("yes")) {
+        alert("You can work")
+    }
+}
+if (age < 20) {
+    alert("Wait a few years")
+}
+if (y == ("no")) {
+    alert("Go to study")
+}
---
+++ main.js
@@ -0,0 +1,13 @@
+function CheckResult() {
+    console.log("Yayyy")
+}
+
+function start() {
+    var p = document.getElementById("myList");
+
+    for (let i = 0; i < 10; i++) {
+        var firstOperand = Math.floor(math.random() = 10);
+        var secondOperand = Math.floor(math.random() = 10);
+        p.innerHTML = p.innerHTML + "<li>" + firstOperand.toString() + "+" + secondOperand.toString() + "<li>"
+    }
+}
